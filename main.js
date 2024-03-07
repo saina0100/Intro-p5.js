99173a895ea855d2482501f10c25c7bed5539c0b
saina0100 <111421414+saina0100@users.noreply.github.com>
2024-03-07 18:08:57 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+
+}
+function draw(){
+    image(video, 230,150,220,200);
+    tint(tint_color);
+fill(255,0,0);
+stroke(255,0,0);
+circle(50,50,80);
+circle(50,430,80);
+circle(590,430,80);
+circle(590,50,80);
+fill(240,180,200);
+stroke(240,180,200);
+rect(90,40,460,20);
+rect(90,420,460,20);
+rect(40,90,20,300);
+rect(580,90,20,300);
+
+}
+function take_snapshot(){
+    save('student_name.png');
+
+}
+function filter_tint()
+{
+    tint_color=document.getElementById("color_name").value;
+}
